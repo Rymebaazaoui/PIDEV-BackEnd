4a78bf288ee8094c6bde888e5cc84b6eb74fe03c
Rym Baazaoui <rym.baazaoui@esprit.tn>
2022-07-20 10:57:47 +0200

    add inscriptionformation collection

--- routes/formations.routes.js
+++ routes/formations.routes.js
@@ -5,9 +5,17 @@ const FormationController = require("../controllers/formations.controller");
 router.get('/', FormationController.showAllformations);
 router.post('/api/create', FormationController.create);
 router.post('/api/addType/:id', FormationController.addFormationType);
+router.post('/api/addType/:id', FormationController.addFormationType);
+router.post('/api/addInscription/:id', FormationController.addInscriptionFormation);
 router.delete('/api/deleteFormation/:id', FormationController.deleteFormationById);
 router.put('/api/updateFormation/:id', FormationController.updateFormation);
 router.get('/api/searchFormation/:id', FormationController.searchFormation);
+router.get('/findFormationByStartingDate/:DateDebut', FormationController.findFormationsByDateAfter);
+router.get('/findFormationByEndingDate/:DateFin', FormationController.findFormationsByDateBefore);
+router.get('/findFormationByTitle/:TitreDeFormation', FormationController.findFormationsByTitle);
+router.get('/api/getFormationType', FormationController.showAllFormationsType);
+
+
 //  router.put('/updateFormationtype/:id', FormationController.updateFormationtype);
 
 module.exports = router;
